20b31f1e5443df81c96e38387763cf78314a1aa7
Cernan Bernardo <cernanb@gmail.com>
2021-03-04 16:02:00 -0800

    Define create action

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -8,4 +8,18 @@ class ItemsController < ApplicationController
     item = Item.find(params[:id])
     render json: item
   end
+
+  def create
+    item = Item.new(item_params)
+    if item.save
+      render json: item
+    else
+      # here is where we would render a json error object
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :description, :price)
+  end
 end
